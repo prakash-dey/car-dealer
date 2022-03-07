6228df53a0fcc4f18d81bc427c9745904c749865
Vaibhav 1 <vaibhav1@Vaibhavs-MacBook-Air-2.local>
2022-03-08 03:23:51 +0530

    Updated

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -9,45 +9,46 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import Slide from './Slide';
+import { useState } from 'react';
 
 function Vehicles() {
-    const carImg = ["./image/v1.png","./image/v2.png","./image/v3.png","./image/v4.png" ,"./image/v5.png", "./image/v6.png" ]
+    const [carImg, setCarImg] = useState(["./image/v1.png", "./image/v2.png", "./image/v3.png", "./image/v4.png", "./image/v5.png", "./image/v6.png"])
     return (
         <section class="vehicles" id="vehicles">
 
             <h1 class="heading"> popular <span>vehicles</span> </h1>
             <div class="swiper vehicles-slider">
-            <Swiper
-                // install Swiper modules
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={50}
-                slidesPerView={3}
-                // navigation
-                pagination={{ clickable: true }}
-                pagination={{ clickable: true }}
-                // scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')} className="swiper-wrapper"
-                centeredSlides={true}
-                loop={true}
-                autoplay = {{delay:2000}}
-                breakpoints={{
-                    // when window width is >= 640px
-                    0: {
-                      slidesPerView: 1,
-                    },
-                    // when window width is >= 768px
-                    768: {
-                      slidesPerView: 2,
-                    },
-                    1024: {
-                        slidesPerView: 3,
-                      }
-                  }}
-            >
-                {carImg.map((el) => <SwiperSlide className='slide'> <Slide img={el} />
-                </SwiperSlide>)}
-            </Swiper>
+                <Swiper
+                    // install Swiper modules
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    // navigation
+                    pagination={{ clickable: true }}
+                    pagination={{ clickable: true }}
+                    // scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    onSlideChange={() => console.log('slide change')} className="swiper-wrapper"
+                    centeredSlides={true}
+                    loop={true}
+                    autoplay={{ delay: 2000 }}
+                    breakpoints={{
+                        // when window width is >= 0px
+                        0: {
+                            slidesPerView: 1,
+                        },
+                        // when window width is >= 768px
+                        768: {
+                            slidesPerView: 2,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                        }
+                    }}
+                >
+                    {carImg.map((el, idx) => <SwiperSlide className='slide'> <Slide img={el} key ={idx} />
+                    </SwiperSlide>)}
+                </Swiper>
             </div>
         </section>
 
