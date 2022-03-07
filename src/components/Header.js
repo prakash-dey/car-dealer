afd9b87842b39bebda43bc9ff97eb4e3f7ca86bc
Vaibhav 1 <vaibhav1@Vaibhavs-MacBook-Air-2.local>
2022-03-08 03:15:00 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,36 @@
-import React ,{useState}from 'react'
+import React, { useState } from 'react'
 
 
 function Header(props) {
   const [active, setActive] = useState(false);
-  const [activeClass, setActiveClass] = useState(["",""]);
+  const [activeClass, setActiveClass] = useState(["", ""]);
 
-  const toggleClass=()=>{
-// It will activate deactivate the drawer
+  const toggleClass = () => {
+    // It will activate deactivate the drawer
     setActive(!active);
-    active? setActiveClass(['fa-times','active']):setActiveClass(['','']);
+    active ? setActiveClass(['fa-times', 'active']) : setActiveClass(['', '']);
   }
   return (
-    <> 
-    <header class="header">
+    <>
+      <header class="header">
 
-            <div id="menu-btn" class={`fas fa-bars ${activeClass[0]}`} onClick={toggleClass}></div>
+        <div id="menu-btn" class={`fas fa-bars ${activeClass[0]}`} onClick={toggleClass}></div>
 
-            <a href="#" class="logo"> <span>Car</span>Dekho </a>
+        <a href="#" class="logo"> <span>Car</span>Dekho </a>
 
-            <nav class={`navbar ${activeClass[1]}`}>
-                <a href="#home">Home</a>
-                <a href="#vehicles">Vehicles</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact</a>
-            </nav>
+        <nav class={`navbar ${activeClass[1]}`}>
+          <a href="#home">Home</a>
+          <a href="#vehicles">Vehicles</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </nav>
 
-            <div id="login-btn" onClick={()=>props.handler(true)}>
-                <button class="btn" >login</button>
-                <i class="far fa-user"></i>
-            </div>
+        <div id="login-btn" onClick={() => props.handler(true)}>
+          <button class="btn" >login</button>
+          <i class="far fa-user"></i>
+        </div>
 
-    </header>
+      </header>
     </>
   )
 }
