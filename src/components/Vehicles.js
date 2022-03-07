c134a2e61524dc66ae1fe2260a17fb8e3c2f4cbb
Vaibhav 1 <vaibhav1@Vaibhavs-MacBook-Air-2.local>
2022-03-07 16:33:04 +0530

    Updated

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -23,13 +23,25 @@ function Vehicles() {
                     spaceBetween={50}
                     slidesPerView={3}
                     // navigation
-                    pagination={{el:".swiper-pagination"},{ clickable: true }}
+                    pagination={{ clickable: true },{el:".swiper-pagination"}}
                     // scrollbar={{ draggable: true }}
                     onSwiper={(swiper) => console.log(swiper)}
                     onSlideChange={() => console.log('slide change')} className="swiper-wrapper"
                     centeredSlides={true}
                     loop={true}
-                    autoplay = {{delay:2000}}
+                    breakpoints={{
+                        // when window width is >= 640px
+                        0: {
+                          slidesPerView: 1,
+                        },
+                        // when window width is >= 768px
+                        768: {
+                          slidesPerView: 2,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                          }
+                      }}
                 >
                     {carImg.map((el) => <SwiperSlide className='slide'> <Slide img={el} />
                     </SwiperSlide>)}
