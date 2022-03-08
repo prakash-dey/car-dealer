2a5e1ff5859bd05b81a4a4e17d1c3da77759e97a
Vaibhav 1 <vaibhav1@Vaibhavs-MacBook-Air-2.local>
2022-03-08 11:23:34 +0530

    Updated

--- src/components/ServicesBox.js
+++ src/components/ServicesBox.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function ServicesBox(props) {
     return (
-        <div class="box">
-            <i class={props.icon}></i>
+        <div className="box">
+            <i className={props.icon}></i>
             <h3>{props.service}</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis, nisi.</p>
-            <a href="#" class="btn"> read more</a>
+            <a href="#" className="btn"> read more</a>
         </div>
     )
 }
